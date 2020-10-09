be3f6e8399d64d2496ba0d21b724e3df61122669
Quacktillius <ajayvs@connect.hku.hk>
2020-10-10 02:39:23 +0800

    Created Sample Scene- Stand on red circle to rise. Press (I) to enable post-processing effects (drunk effects, map change etc)

---
+++ Echo_Echo/Assets/Ingest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingest : MonoBehaviour
+{
+    public GameObject box;
+    public GameObject effects;
+    
+    void enablePostEffects()
+    {
+        effects.SetActive(true);
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            enablePostEffects();
+            Destroy(box);
+        }
+    }
+}
---
+++ Echo_Echo/Assets/podium_rise.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class podium_rise : MonoBehaviour
+{
+    public GameObject podium;
+    public Vector3 finalPosition = new Vector3(0, 1, 0);
+    public float speed = 1.0f;
+    public bool rise = false;
+
+    IEnumerator OnTriggerEnter(Collider collider)
+    {
+        yield return new WaitForSeconds(3);
+        rise = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rise)
+            podium.transform.position = Vector3.Lerp(podium.transform.position, finalPosition, speed * Time.deltaTime);
+    }
+}
